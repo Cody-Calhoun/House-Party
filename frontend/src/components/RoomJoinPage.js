aa2eb9c3a95a08175ce9d68461daa2d54f0e590d
Will <w.calhoun@outlook.com>
2020-12-21 22:38:09 -0800

    Added Material UI and api endpoints

---
+++ frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "React";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+export default class RoomJoinPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>This is the RoomJoinPage!</p>;
+  }
+}
